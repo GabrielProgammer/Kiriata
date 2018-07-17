b604e86385df9db086f1ba546396200a78c3599f
Gabriel Victor <gabriel.victor.ti@gmail.com>
2018-07-17 17:22:53 -0300

    opa

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -9,46 +9,42 @@ import { Storage } from '@ionic/storage';
   See https://angular.io/guide/dependency-injection for more info on providers
   and Angular DI.
 */
-
 @Injectable()
 export class ApiProvider {
 	private apiKey: string;
   constructor(public http: HttpClient, public storage: Storage) {
   	this.apiKey = "4e47dbe7f1ea88e915d595fcb76c6170";
+    console.log('Hello ApiProvider Provider');
   }
 
-  getFilmesPopular(): any { //Pega os filmes mais populares do dia
+  getFilmesPopular(): any {
   	return this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=' + this.apiKey
   	 + "&language=pt-BR&page=1");
+  	
   }
 
-  getCategorias(): any { //Pega todos os gêneros
+  getCategorias(): any {
   	return  this.http.get<Filme>('htps://api.themoviedb.org/3/genre/movie/list?api_key=' + this.apiKey
-  		+ "&language=pt-BR");
+  		+"&language=pt-BR");
   }
 
-  getPesquisa(termo): any { //Faz uma pesquisa baseado no termo inserido
+  getPesquisa(termo): any {
     return this.http.get('https://api.themoviedb.org/3/search/movie?api_key=' + this.apiKey +
       '&language=pt-BR&query=' + termo +'&page=1&include_adult=true');
   }
 
-  getMovie(id): any { //Pega as informações do filme informado
+  getMovie(id): any {
      return this.http.get('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + this.apiKey +
       '&language=pt-BR');
   }
 
-  /*
-    PARAM listadoPor = Allowed Values: , popularity.asc, popularity.desc, release_date.asc, release_date.desc, revenue.asc, 
-    revenue.desc, primary_release_date.asc, primary_release_date.desc, original_title.asc, original_title.desc, 
-    vote_average.asc, vote_average.desc, vote_count.asc, vote_count.desc, DEFAULT: popularity.desc
-  */
-  getDiscoverMovie(genero, page?, listadoPor?):any { //Lista os filmes do gênero informado 
+  getDiscoverMovie(genero, page?):any {
     return this.http.get('https://api.themoviedb.org/3/discover/movie?api_key=' + this.apiKey +
-      '&language=pt-br&sort_by=' + listadoPor + '&include_adult=false&include_video=false&page='+ page +'&with_genres='
-      + genero);
+    '&language=pt-br&sort_by=popularity.desc&include_adult=false&include_video=false&page='+ page +'&with_genres='
+    + genero);
   }
 
-  getGeneros() { //Lista todos os gêneros 
+  getGeneros() {
     return this.http.get('https://api.themoviedb.org/3/genre/movie/list?api_key=' + this.apiKey + 
       '&language=pt-br');
   }
